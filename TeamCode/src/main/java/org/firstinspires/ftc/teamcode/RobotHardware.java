243488e44894a7a06ff24e48ed8cf0e3d16b5fa8
halftonenano <86031051+halftonenano@users.noreply.github.com>
2023-11-16 15:49:13 -0800

    new odometry testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -1,17 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RobotHardware {
-    public DcMotor lbDrive = null;
-    public DcMotor lfDrive = null;
-    public DcMotor rbDrive = null;
-    public DcMotor rfDrive = null;
-
     HardwareMap hwMap = null;
+
+    public DcMotor lbDrive;
+    public DcMotor lfDrive;
+    public DcMotor rbDrive;
+    public DcMotor rfDrive;
+    public DcMotor rail;
+
+    // For Odometry
+    // 35mm omniwheel = 1.37795 inches
+    public final double COUNTS_PER_ROTATION = 1024;
+    public final double ROTATION_PER_INCH = 1.0 / (1.37795 * Math.PI);
+    public final double COUNTS_PER_INCH = COUNTS_PER_ROTATION * ROTATION_PER_INCH; // CPR * RPI
+    public DcMotorEx encoderL;
+    public DcMotorEx encoderR;
+
     private ElapsedTime period = new ElapsedTime();
 
     public RobotHardware() {}
@@ -20,27 +31,35 @@ public class RobotHardware {
         // Save reference to Hardware map
         hwMap = ahwMap;
 
+        // Initialize Odometry Hardware
+        encoderL = hwMap.get(DcMotorEx.class, "encoderL");
+        encoderR = hwMap.get(DcMotorEx.class, "encoderR");
+
         // Define and Initialize Motors
         lfDrive = hwMap.get(DcMotor.class, "motorFL");
         lbDrive = hwMap.get(DcMotor.class, "motorBL");
         rfDrive = hwMap.get(DcMotor.class, "motorFR");
         rbDrive = hwMap.get(DcMotor.class, "motorBR");
+        rail = hwMap.get(DcMotor.class, "railRAIL");
 
         lfDrive.setDirection(DcMotor.Direction.FORWARD);
         lbDrive.setDirection(DcMotor.Direction.FORWARD);
         rfDrive.setDirection(DcMotor.Direction.FORWARD);
         rbDrive.setDirection(DcMotor.Direction.FORWARD);
+        rail.setDirection(DcMotor.Direction.FORWARD);
 
         // Set all motors to zero power
         lfDrive.setPower(0);
         lbDrive.setPower(0);
         rfDrive.setPower(0);
         rbDrive.setPower(0);
+        rail.setTargetPosition(0);
 
         lfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         lbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rail.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         lfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopLite.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopLite.java
@@ -9,9 +9,10 @@ public class TeleopLite extends OpMode {
     /* Declare OpMode members. */
     RobotHardware robot = new RobotHardware(); // use the class created to define a Pushbot's hardware
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+    double odometryLinitial;
+    double odometryRinitial;
+
+    // Code to run ONCE when the driver hits INIT
     @Override
     public void init() {
         /*
@@ -21,76 +22,67 @@ public class TeleopLite extends OpMode {
         robot.init(hardwareMap);
         // Send telemetry message to signify robot waiting
         telemetry.addData("Say", "Hello thomas");
+
+        odometryLinitial = robot.encoderL.getCurrentPosition();
+        odometryRinitial = robot.encoderR.getCurrentPosition();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
+    // Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
     @Override
     public void init_loop() {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+    // Code to run ONCE when the driver hits PLAY
     @Override
     public void start() {
+
     }
 
-    /*
-     * This is Jonathan's code for fine movement
-     * If the robot isn't working try deleting this
-     * Update: this code is perfect and deleting it is a sin
-     */
-    double movemult = 1.0;
+    final double joystickBaseSpeed = 0.3;
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
     public void loop() {
-        // Gamepad Inputs
-        float gp1_throttle = gamepad1.left_stick_y;
-        float gp1_strafe = gamepad1.left_stick_x;
-        float gp1_yaw = gamepad1.right_stick_x;
-
-        boolean dpL = gamepad1.dpad_left;
-        boolean dpR = gamepad1.dpad_right;
-        boolean dpU = gamepad1.dpad_up;
-        boolean dpD = gamepad1.dpad_down;
-
         // Final Robot Instructions
-        double final_throttle = -gp1_throttle;
-        double final_strafe = gp1_strafe;
-        float final_yaw = gp1_yaw;
+        double final_throttle = 0.0;
+        double final_strafe = 0.0;
+        double final_yaw = 0.0;
         
-        // D-pad movement
-        if (dpL) {
-            final_strafe -= 0.5; 
-        }
-        if (dpR) {
-            final_strafe += 0.5; 
-        }
-        if (dpU) {
-            final_throttle -= 0.5; 
-        }
-        if (dpD) {
-            final_throttle += 0.5; 
-        }
-        
-        movemult = 0.5 + gamepad1.right_trigger;
+        double joystickMultiplier = joystickBaseSpeed + (1.0 - gamepad1.right_trigger);
 
-        robot.lfDrive.setPower((final_throttle - final_strafe - final_yaw) * 0.7 * movemult);
-        robot.lbDrive.setPower((final_throttle + final_strafe - final_yaw) * 0.7 * movemult);
-        robot.rfDrive.setPower((-final_throttle - final_strafe - final_yaw) * 0.7 * movemult);
-        robot.rbDrive.setPower((-final_throttle + final_strafe - final_yaw) * 0.7 * movemult);
+        // Movement speed for IDCSC (Intelligent Dynamic Cruise Speed Control)
+        double idcscSpeed = gamepad1.right_trigger;
+        
+        final_throttle += (gamepad1.left_stick_y * joystickMultiplier);
+        final_strafe += (gamepad1.left_stick_x * joystickMultiplier);
+        final_yaw += (gamepad1.right_stick_x * joystickMultiplier);
+
+        // D-pad movement
+        if (gamepad1.dpad_left) {
+            final_strafe -= idcscSpeed;
+        }
+        if (gamepad1.dpad_right) {
+            final_strafe += idcscSpeed;
+        }
+        if (gamepad1.dpad_down) {
+            final_throttle += idcscSpeed;
+        }
+        if (gamepad1.dpad_up) {
+            final_throttle -= idcscSpeed;
+        }
+        
+        robot.lfDrive.setPower(final_throttle - final_strafe - final_yaw);
+        robot.lbDrive.setPower(final_throttle + final_strafe - final_yaw);
+        robot.rfDrive.setPower(-final_throttle - final_strafe - final_yaw);
+        robot.rbDrive.setPower(-final_throttle + final_strafe - final_yaw);
+
+        telemetry.addData("OdometryL: ", (odometryLinitial - robot.encoderL.getCurrentPosition()) / robot.COUNTS_PER_INCH);
+        telemetry.addData("OdometryR: ", (odometryRinitial - robot.encoderR.getCurrentPosition()) / robot.COUNTS_PER_INCH);
 
         telemetry.update();
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
+    // Code to run ONCE after the driver hits STOP
     @Override
     public void stop() {
     }
