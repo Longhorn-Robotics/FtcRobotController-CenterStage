d4aeed69c2c9ba202aae5264be32428169cbd98c
Anticubex <teofontana00@gmail.com>
2024-12-05 15:45:20 -0800

    Grouped the motors together, applied the stuff from the test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardwareSIGMA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardwareSIGMA.java
@@ -1,11 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import java.util.function.Consumer;
 // import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class RobotHardwareSIGMA {
@@ -14,36 +11,15 @@ public class RobotHardwareSIGMA {
     public DcMotor lfDrive;
     public DcMotor rbDrive;
     public DcMotor rfDrive;
-    public DcMotor bucketRail1;
-    public DcMotor bucketRail2;
+    public DcMotor bucketRailL;
+    public DcMotor bucketRailR;
     public Servo bucket;
     public Servo clawExtend;
     public Servo clawPivot;
     public Servo clawPinch;
 
-    // Note: Maybe pull this outside?
-    static class DualMotor {
-        DcMotor motor1;
-        DcMotor motor2;
-        public DualMotor(DcMotor _motor1, DcMotor _motor2) {
-            motor1 = _motor1;
-            motor2 = _motor2;
-        }
-
-        public void setTargetPosition(int target) {
-            apply((DcMotor a) -> a.setTargetPosition(target));
-        }
-        public void setMode(DcMotor.RunMode mode) {
-            apply((DcMotor a) -> a.setMode(mode));
-        }
-
-        public void apply(Consumer<DcMotor> function) {
-            function.accept(motor1);
-            function.accept(motor2);
-        }
-    }
-
-    DualMotor railMotors;
+    GroupMotor railMotors;
+    GroupMotor wheels;
 
 //    private ElapsedTime period = new ElapsedTime();
 
@@ -58,31 +34,24 @@ public class RobotHardwareSIGMA {
         lbDrive = hwMap.get(DcMotor.class, "motorBL");
         rfDrive = hwMap.get(DcMotor.class, "motorFR");
         rbDrive = hwMap.get(DcMotor.class, "motorBR");
-        lfDrive.setDirection(DcMotor.Direction.FORWARD);
-        lbDrive.setDirection(DcMotor.Direction.FORWARD);
-        rfDrive.setDirection(DcMotor.Direction.FORWARD);
-        rbDrive.setDirection(DcMotor.Direction.FORWARD);
-        lfDrive.setPower(0);
-        lbDrive.setPower(0);
-        rfDrive.setPower(0);
-        rbDrive.setPower(0);
-        lfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        lfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rfDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rbDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        wheels = new GroupMotor(lfDrive, lbDrive, rfDrive, rbDrive);
+
+        wheels.apply(dcMotor -> {
+            dcMotor.setDirection(DcMotor.Direction.FORWARD);
+            dcMotor.setPower(0);
+            dcMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            dcMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        });
 
         // Initialize linear rail motor to run with encoder
-        bucketRail1 = hwMap.get(DcMotor.class, "railRAIL");
-        bucketRail2 = hwMap.get(DcMotor.class, "railRAIL");
+        bucketRailL = hwMap.get(DcMotor.class, "railL");
+        bucketRailR = hwMap.get(DcMotor.class, "railR");
+        bucketRailL.setDirection(DcMotor.Direction.FORWARD);
+        bucketRailR.setDirection(DcMotor.Direction.REVERSE);
 
-        railMotors = new DualMotor(bucketRail1, bucketRail2);
+        railMotors = new GroupMotor(bucketRailL, bucketRailR);
 
         railMotors.apply((DcMotor motor) -> {
-            motor.setDirection(DcMotor.Direction.REVERSE);
             motor.setTargetPosition(0);
             motor.setPower(0.8);
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
