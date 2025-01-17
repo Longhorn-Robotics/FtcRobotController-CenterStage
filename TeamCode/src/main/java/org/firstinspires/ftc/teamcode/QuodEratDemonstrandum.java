f50b6a4b4dcdb0c3f89be0829b77211156a50df1
halftone <86031051+halftonenano@users.noreply.github.com>
2025-01-16 21:10:43 -0800

    autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QuodEratDemonstrandum.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.concurrent.TimeUnit;
+
+// >> Experimental
+
+public class QuodEratDemonstrandum {
+    private RobotHardwareSIGMA robot;
+
+    private int wheelBL = 0;
+    private int wheelFL = 0;
+    private int wheelBR = 0;
+    private int wheelFR = 0;
+
+    private int railPosition = 0;
+
+    public QuodEratDemonstrandum(RobotHardwareSIGMA robot) {
+        this.robot = robot;
+    }
+
+    public void driveY(int value) {
+        wheelBL += value;
+        wheelFL += value;
+        wheelBR -= value;
+        wheelFR -= value;
+    }
+
+    public void driveX(int value) {
+        wheelBL += value;
+        wheelFL -= value;
+        wheelBR += value;
+        wheelFR -= value;
+    }
+
+    public void rotate(int degrees) {
+        wheelBL += (int) (degrees * 0.5);
+        wheelFL += (int) (degrees * 0.5);
+        wheelBR += (int) (degrees * 0.5);
+        wheelFR += (int) (degrees * 0.5);
+    }
+
+    public void commit() {
+        // set all targets
+        robot.lbDrive.setTargetPosition(wheelBL);
+        robot.lfDrive.setTargetPosition(wheelFL);
+        robot.rbDrive.setTargetPosition(wheelBR);
+        robot.rfDrive.setTargetPosition(wheelFR);
+        robot.bucketRailL.setTargetPosition(railPosition);
+        robot.bucketRailR.setTargetPosition(railPosition);
+
+        while (
+                Math.abs(robot.lfDrive.getCurrentPosition() - wheelFL) > 5 || Math.abs(robot.lbDrive.getCurrentPosition() - wheelBL) > 5
+                        || Math.abs(robot.rbDrive.getCurrentPosition() - wheelBR) > 5 || Math.abs(robot.rfDrive.getCurrentPosition() - wheelFR) > 5
+                        || Math.abs(robot.bucketRailL.getCurrentPosition() - railPosition) > 5 || Math.abs(robot.bucketRailR.getCurrentPosition() - railPosition) > 5
+        ) {
+            // wait...
+        }
+
+        return;
+    }
+
+    public void pause(int ms) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(ms);
+        } catch(InterruptedException e) {}
+    }
+
+    public void init() {
+        robot.wheels.apply(motor -> {
+            motor.setTargetPosition(0);
+            motor.setPower(0.5);
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        });
+    }
+}
